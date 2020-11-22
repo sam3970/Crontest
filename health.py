88b347d611c131fd57d2acf6ec62d26b92cc4f44
SangKyu Lee <sam3970@gmail.com>
2020-11-22 17:48:35 +0900

    class 화 시켜서 수정 개발

--- health.py
+++ health.py
@@ -7,50 +7,74 @@ import subprocess
 import requests
 from urllib.parse import urlencode
 
-# 기준 및 초기값 설정
-cpuLimit = 15 # cpu 사용량
-memLimit = 15 # 메모리 사용량
+class Health :
 
-serverName = 'sklee_02'
-title = '[' + serverName + ' server]\r\n'
-msg = title
+	#def __init__(self) :
+		#self.cpuLimit = 15; #cpu 사용량
+		#self.memLimit = 15; #memory 사용량
+		#self.memUsage = memUsage;
+		#self.avgCpu = avgCpu;
 
-def send(chat):
-    print(chat)
-    params1 =  urlencode({'chat_id': '921889996', 'text': chat}).encode()
-    r = requests.post('https://api.telegram.org/bot937768411:AAFbP6he1iZtWpTNvfAVutv_TdO8vSdMk2g/sendMessage',  params=params1, json=[])
-    print(r.elapsed.total_seconds())
-    print(r.json())
+	# 기준 및 초기값 설정
+	cpuLimit = 15 # cpu 사용량
+	memLimit = 15 # 메모리 사용량
 
-# cpu 사용율 계산
-def getCpuUsage():
-    cpu = 0
-    for x in range(2):
-        cpu += psutil.cpu_percent(interval=1)
-        return round(float(cpu)/3,2)
 
-# memory 사용량 계산
-def getMemUsage():
-    mem=str(os.popen('free -t -m').readlines())
-    T_ind=mem.index('T')
-    mem=mem[T_ind+6:]
-    mem_T=mem[:13]
-    mem_sub=mem[14:]
-    mem_U=mem_sub[:13]
-    return round(float(mem_U)/float(mem_T)*100,2)
+	serverName = 'sklee_02'
+	title = '[' + serverName + ' server]\r\n'
+	msg = title
 
-avgCpu = getCpuUsage()
-memUsage = getMemUsage()
+	def send(self,chat):
+		print(chat)
+		params1 =  urlencode({'chat_id': '921889996', 'text': chat}).encode()
+		r = requests.post('https://api.telegram.org/bot937768411:AAFbP6he1iZtWpTNvfAVutv_TdO8vSdMk2g/sendMessage',  params=params1, json=[])
+		print(r.elapsed.total_seconds())
+		print(r.json())
 
-if avgCpu > cpuLimit:                                                                                                      
-    msg += 'cpu(' + str(cpuLimit) + '%) exceed!.\r\n'         
-    msg += 'cpu usage : ' + str(avgCpu) + '%\r\n'
+	# cpu 사용율 계산
+	def getCpuUsage(self):
+		cpu = 0
+		for x in range(2):
+			cpu += psutil.cpu_percent(interval=1)
+			return round(float(cpu)/3,2)
 
-if memUsage > memLimit:
-    msg += 'memory(' + str(memLimit) + '%) exceed!.\r\n'
-    msg += 'memory usage : ' + str(memUsage) + '%'
+	# memory 사용량 계산
+	def getMemUsage(self):
+		mem=str(os.popen('free -t -m').readlines())
+		T_ind=mem.index('T')
+		mem=mem[T_ind+6:]
+		mem_T=mem[:13]
+		mem_sub=mem[14:]
+		mem_U=mem_sub[:13]
+		return round(float(mem_U)/float(mem_T)*100,2)
 
-if avgCpu > cpuLimit or memUsage > memLimit :
-    send(msg)
+	#print(memUsage)
+	#print(avgCpu)
+
+heal = Health()
+
+memUsage = heal.getMemUsage()
+avgCpu = heal.getCpuUsage()
+
+#print(memUsage)
+
+if avgCpu > heal.cpuLimit:
+	heal.msg += 'cpu(' + str(heal.cpuLimit) + '%) exceed!.\r\n'
+	heal.msg += 'cpu usage : ' + str(avgCpu) + '%\r\n'
+	print('1')
+	
+if memUsage > heal.memLimit:
+	heal.msg += 'memory(' + str(heal.memLimit) + '%) exceed!.\r\n'
+	heal.msg += 'memory usage : ' + str(memUsage) + '%'
+	print('2')
+	
+if avgCpu > heal.cpuLimit or memUsage > heal.memLimit:
+	heal.send(heal.msg)
+	print('3')
+	
 else:
 	print("CPU, Memory가 정상수치를 유지하고 있습니다.")
+
+########## main method check part(similar initializing) ##########
+#if __name__ == "__main__" :
+#    main()
